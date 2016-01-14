61cb025a330f64da2c2c01b8780dbcfb5251c26a
Sunny Gulati <sunnywiz@gmail.com>
2016-01-14 18:14:19 -0500

    Added basic website.
    Next: move ALL the code over.

---
+++ DotNetMud/DotNetMud.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DotNetMud.Web.Startup))]
+namespace DotNetMud.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
