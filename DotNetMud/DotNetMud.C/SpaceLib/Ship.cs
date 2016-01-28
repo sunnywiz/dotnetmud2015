8c1084774b18fd6cec4dddf836c1ea999b300a4c
Sunny Gulati <sunnywiz@gmail.com>
2016-01-28 18:55:25 -0500

    WIP - needs some refactoring.   Next 10 steps drafted.

--- DotNetMud/DotNetMud.C/SpaceLib/Ship.cs
+++ DotNetMud/DotNetMud.C/SpaceLib/Ship.cs
@@ -17,7 +17,20 @@ namespace DotNetMud.C.SpaceLib
 
         public object RequestPoll(string pollName, object clientState)
         {
-            return new PollResult1();
+            return new PollResult1()
+            {
+                // WHERE ARE WE LEAVING OFF. 
+                // we need a smaller DTO for geometry that doesn't have the Container.  
+                // I think the Container is a SpaceLibObject that also implments the interface. 
+                // then we need to provie all the geometry for all the objects in the pollresult. 
+                // then we need to echo that information on the client
+                // then we test two people logging in
+                // then we need to put a canvas on the client .. when we get an update, draw the update
+                // then we need to set up an animation loop on the server
+                // then we need to see the client get updated in a choppy way 
+                // then we need the animation loop on the client to interpolate in a smooth way
+                // then we need to send thrust info back to the server
+            };
         }
 
         public Space2D Container { get; set; }
--- DotNetMud/DotNetMud.C/SpaceLib/SpaceGameSpecifics.cs
+++ DotNetMud/DotNetMud.C/SpaceLib/SpaceGameSpecifics.cs
@@ -5,18 +5,54 @@ namespace DotNetMud.C.SpaceLib
 {
     public class SpaceGameSpecifics : IGameSpecifics
     {
+        private static int playerNumber = 0; 
         public IInteractive CreateNewPlayer()
         {
-            return Driver<SpaceGameSpecifics>.Instance.CreateNewStdObject("assembly://DotNetMud.C//DotNetMud.C.SpaceLib.Ship") as IInteractive;
+            var player =
+                Driver<SpaceGameSpecifics>.Instance.CreateNewStdObject(
+                    "assembly://DotNetMud.C//DotNetMud.C.SpaceLib.Ship");
+            var p2 = player as IInteractive;
+            var p3 = player as IObject2D;
+
+            if (p3 != null)
+            {
+                var r = new Random();
+                p3.X = r.NextDouble()*300.0 - 100.0;
+                p3.Y = r.NextDouble()*300.0 - 100.0;
+                p3.R = r.NextDouble()*Math.PI*2; 
+                p3.DX = r.NextDouble() * 10.0 - 5.0;
+                p3.DY = r.NextDouble() * 10.0 - 5.0;
+                p3.DR = r.NextDouble() * 10.0 - 5.0;
+                p3.Name = "P" + (++playerNumber);
+                p3.Image = "Ship1";
+                // TODO: will probably need a sizing thing for the image as well. 
+            }
+            return p2; 
         }
 
         public void WelcomeNewPlayer(IInteractive newPlayer)
         {
-            return; 
+            var x = newPlayer as IObject2D;
+            if (x == null) return; 
+            var space =
+                Driver<SpaceGameSpecifics>.Instance.FindSingletonByUri(
+                    "assembly://DotNetMud.C/DotNetMud.C.SpaceLib.Space2D") as Space2D;
+            if (space == null) return; 
+            space.Objects.Add(x);
+            x.Container = space; 
         }
 
         public void PlayerGotDisconnected(IInteractive playerObject, bool wasItIntentional)
         {
+            var x = playerObject as IObject2D;
+            if (x == null) return;
+            var space =
+                Driver<SpaceGameSpecifics>.Instance.FindSingletonByUri(
+                    "assembly://DotNetMud.C/DotNetMud.C.SpaceLib.Space2D") as Space2D;
+            if (space == null) return;
+            space.Objects.Remove(x);
+            x.Container = null; 
+            Driver<SpaceGameSpecifics>.Instance.RemoveStdObjectFromGame(playerObject as StdObject);
         }
     }
 }
